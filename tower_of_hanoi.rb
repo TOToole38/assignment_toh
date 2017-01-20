1afffe6bfd14ad656b7f8cfeb3961e95a429a4b8
totoole38 <totoole38@gmail.com>
2017-01-20 11:10:14 -0700

    clean up code and add comments

--- tower_of_hanoi.rb
+++ tower_of_hanoi.rb
@@ -1,4 +1,4 @@
-#  There are 3 pegs. 
+# There are 3 pegs. 
 # Disks begin on 1 peg with the largest disk at the bottom and the smallest at the top. 
 
 # Rules: 
@@ -7,20 +7,25 @@
 # No disk may be placed on top of a smaller disk 
 
 
-def setup_game
-	# Instructions
-	puts "Welcome to the Tower of Hanoi!"
-	puts "There are 3 pegs and 3 disks. Currently all the disks are on peg number 1."
-	puts "Goal: Get all the disks from peg number 1 onto either peg number 2 or 3."
-	puts "Move a disk by specifying which peg number it is coming from and which peg number it is going to i.e. 1, 3"
-	puts "Type 'quit' to exit the game."
-	puts " "
 
-	# Initial game structure 
+# 
+# 
+# Kick off the game with the instructions and the starting disk placement
+def setup_game
+	puts "\nWelcome to the Tower of Hanoi!"
+	puts "\tAll 3 of the disks will start on peg number 1."
+	puts "\tGoal: Get all the disks from peg number 1 onto either peg number 2 or 3."
+	puts "\tMove a disk by specifying which peg number it is coming from and which peg number it is going to i.e. 1, 3 \n \n"
+	puts "Type 'summary' to see a visual of the current game state. \nType 'quit' to exit the game. \n \n"
+
 	return [[3, 2, 1], [], []] 
 end
 
 
+
+# Check to make sure the user input is formatted correctly. 
+# If not, the error message will be shown and they will be prompted
+# to enter another input. 
 def format_check(user_input, from_peg, to_peg)
 	if (user_input.length == 2 && from_peg.between?(0,2) && to_peg.between?(0,2))
 		return true
@@ -31,9 +36,11 @@ def format_check(user_input, from_peg, to_peg)
 end
 
 
+
+# Verify the requested move is legal and possible.  
+# If not, an error will be shown and the user will go again. 
 def legal_move(peg_state, from_peg, to_peg)
 	if peg_state[to_peg].length == 0 || peg_state[to_peg] == [nil]
-		# print "empty to "
 		return true
 	elsif peg_state[from_peg].length == 0
 		puts "There are no pegs on peg number #{from_peg+1}."
@@ -46,12 +53,20 @@ def legal_move(peg_state, from_peg, to_peg)
 	end
 end
 
+
+
+
+# Execute the moving of the disk from one peg to aother. 
 def move_disk(peg_state, from_peg, to_peg)
  	peg_state[to_peg] << peg_state[from_peg].last
  	peg_state[from_peg].pop 
  return peg_state
 end
 
+
+
+# After the disk is moved, the program checks to see if the user has won. 
+# If either peg 2 or 3 has all three disks on it
 def check_if_won(peg_state)
 	if peg_state[1].length == 3 || peg_state[2].length == 3
 		return true
@@ -60,15 +75,15 @@ def check_if_won(peg_state)
 	end
 end
 
+ 
 
+# Show a visual representation of where the disks are relative to their pegs 
 def render (peg_state)
 	line = 2
-	
 	while line >= 0
 		puts ("o"*peg_state[0][line].to_i).ljust(10) + ("o"*peg_state[1][line].to_i).center(10) + ("o"*peg_state[2][line].to_i).rjust(10)
-		line = line - 1
+		line -= 1
 	end
-
 
 	puts "-" * 30 
 	puts "Peg 1".ljust(10) + "Peg 2".center(10) + "Peg 3".rjust(10)
@@ -77,47 +92,41 @@ end
 
 
 
-
-
-
-
-
-
+# Main script method. 
+# Gets user input, checks for quit or summary keywords, and validates
+# format and validity of user request before executing the move. 
 def play 
-	peg_state = setup_game   # [[3, 2, 1], [], []] 
-	# print peg_state
-
+	peg_state = setup_game  
 	continue_game = true
 
 	while continue_game
 		print "Enter a move: "
-		user_input = gets.chomp.strip.split(",")
-		# puts user_input
+		user_input = gets.chomp.strip.downcase.split(",")
 		from_peg = user_input[0].to_i - 1
-		# puts from_peg
 		to_peg = user_input [1].to_i - 1
-		# puts to_peg
 
 		if user_input == ["quit"]
 			exit
+		elsif user_input == ["summary"]
+			render(peg_state)
 		elsif (format_check(user_input, from_peg, to_peg) == false) || (legal_move(peg_state, from_peg, to_peg) == false)
 			redo
 		else
 			peg_state = move_disk(peg_state, from_peg, to_peg)
 		end
 
-		render(peg_state)
-
 		if check_if_won(peg_state)
 			puts "Congratulations!! You figured out the Tower of Hanoi!"
 			continue_game = false 
 		end
 	end
-
-	puts "exiting game"
 	exit
 end
 
 
-
+# Play the game! 
 play
+
+
+
+
